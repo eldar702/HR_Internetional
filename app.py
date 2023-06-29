6a1d90937d99a5b6557759fcb0b2318782c0830f
eldar shlomi <eldarshlomi7@gmail.com>
2023-06-29 19:38:32 +0300

    with prediction and ugly sidebar

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, redirect
 from werkzeug.utils import secure_filename
 import os
 import parse
 import highlight
+import visualization
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads/', filename))
-        parse.parse_to_csv(file)
-        return redirect(url_for('highlight_words'))
     return render_template('upload.html')
 
-@app.route('/highlight', methods=['GET', 'POST'])
+@app.route('/highlight')
 def highlight_words():
-    if request.method == 'POST':
-        return highlight.highlight_important_words()
     return render_template('highlight.html')
 
-@app.route('/visualization', methods=['GET', 'POST'])
-def visualization():
-    if request.method == 'POST':
-        function_name = request.form['function_name']
-        return redirect(url_for(function_name))
+@app.route('/visualization')
+def visualization_page():
     return render_template('visualization.html')
+
+@app.route('/prediction')
+def prediction_page():
+    return render_template('prediction.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # Load the model
+    model = pickle.load(open('XGBoost_model.sav', 'rb'))
+
+    # Assume that parsed_text is the input for your model
+    parsed_text = parse.parse_resume()  # You need to implement this function
+
+    # Make a prediction
+    prediction = model.predict(parsed_text)
+
+    # Render the prediction page with the prediction
+    return render_template('prediction.html', prediction=prediction)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ visualization.py
@@ -0,0 +1,19 @@
+def visualization_1():
+    # Insert your code here
+    pass
+
+def visualization_2():
+    # Insert your code here
+    pass
+
+def visualization_3():
+    # Insert your code here
+    pass
+
+def visualization_4():
+    # Insert your code here
+    pass
+
+def visualization_5():
+    # Insert your code here
+    pass
