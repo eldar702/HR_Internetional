312bec7b0d161a87c434d07e5ccb803a1a2bbfa3
eldar shlomi <eldarshlomi7@gmail.com>
2023-07-09 18:01:22 +0300

    final

--- visualization.py
+++ visualization.py
@@ -1,63 +1,42 @@
 import os
 import json
-
-import Utils
 import highlight
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 
-# def create_visualization(filename):
-#     _, skills = highlight.highlight_important_words(filename)
-#
-#     # Prepare the skills hierarchy for the sunburst chart
-#     skills_hierarchy = {"name": "skills", "children": []}
-#     skills_hierarchy["children"].append({"name": "Candidate Skills", "children": [{"name": skill} for skill in skills]})
-#     # Save the skills hierarchy as a JSON file
-#     with open('static/data/skills.json', 'w') as file:
-#         json.dump(skills_hierarchy, file)
-#
-#     # load the skills.json file
-#     with open('static/data/skills.json', 'r') as f:
-#         data = json.load(f)
-#
-#     def process_dict(d):
-#         return {"name": d["name"], "value": len(d["children"]) if "children" in d else 1}
-#
-#     def dict_to_list(d):
-#         res = [process_dict(d)]
-#         if "children" in d:
-#             for child in d["children"]:
-#                 res.extend(dict_to_list(child))
-#         return res
-#
-#     data_list = dict_to_list(data)
-#
-#     names = [item['name'] for item in data_list]
-#     sizes = [item['value'] for item in data_list]
-#
-#     cmap = plt.get_cmap("tab20c")
-#     outer_colors = cmap(np.arange(3)*4)
-#     inner_colors = cmap(np.array([1, 2, 5, 6, 9, 10]))
-#
-#     plt.pie(sizes, labels=names, colors=inner_colors, startangle=90, counterclock=False, radius=1.0)
-#     plt.savefig("static/images/sunburst.png", format="png")
-
 def create_visualization(filename):
-    # Using the res dictionary directly as skills
-    res = Utils.res
+    _, skills = highlight.highlight_important_words(filename)
 
-    # Create the sizes and names lists from res
-    names = list(res.keys())
-    sizes = [len(v) for v in res.values()]
+    # Prepare the skills hierarchy for the sunburst chart
+    skills_hierarchy = {"name": "skills", "children": []}
+    skills_hierarchy["children"].append({"name": "Candidate Skills", "children": [{"name": skill} for skill in skills]})
+    # Save the skills hierarchy as a JSON file
+    with open('static/data/skills.json', 'w') as file:
+        json.dump(skills_hierarchy, file)
 
-    # Generate colors
+    # load the skills.json file
+    with open('static/data/skills.json', 'r') as f:
+        data = json.load(f)
+
+    def process_dict(d):
+        return {"name": d["name"], "value": len(d["children"]) if "children" in d else 1}
+
+    def dict_to_list(d):
+        res = [process_dict(d)]
+        if "children" in d:
+            for child in d["children"]:
+                res.extend(dict_to_list(child))
+        return res
+
+    data_list = dict_to_list(data)
+
+    names = [item['name'] for item in data_list]
+    sizes = [item['value'] for item in data_list]
+
     cmap = plt.get_cmap("tab20c")
-    colors = cmap(np.arange(len(names)))
+    outer_colors = cmap(np.arange(3)*4)
+    inner_colors = cmap(np.array([1, 2, 5, 6, 9, 10]))
 
-    plt.pie(sizes, labels=names, colors=colors, startangle=90, counterclock=False)
-    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.pie(sizes, labels=names, colors=inner_colors, startangle=90, counterclock=False, radius=1.0)
     plt.savefig("static/images/sunburst.png", format="png")
-
-# Call the function to create the visualization
-create_visualization(filename="cv.pdf")
